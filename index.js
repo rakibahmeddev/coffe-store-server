e0f916e6d571d0cf7f0dcd116ae031ea85f130f2
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-19 14:07:11 +0600

    mongodb connected and .env file created to protect sharing sensetive information to github and also used .gitignore to not sending .env and node_modules to github

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const PORT = process.env.PORT || 3000;  
+const PORT = process.env.PORT || 3000;
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
-app.get ('/', (req, res)=> {
-    res.send('Welcome to the Coffee Store API');
-})
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
+// MongoDB connection UR
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@coffestore.dknjp1m.mongodb.net/?retryWrites=true&w=majority&appName=CoffeStore`;
+
+  console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Coffee Store API');
+});
 
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
